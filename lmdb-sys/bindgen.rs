vorot93/libmdbx-rs

commit dccf69c306d22275636eb1fad9b9ed47877343f4

Update bindgen. (#76)

lmdb-sys/bindgen.rs
@@ -50,6 +50,7 @@ pub fn generate() {
         .whitelist_var("^(MDB|mdb)_.*")
         .whitelist_type("^(MDB|mdb)_.*")
         .whitelist_function("^(MDB|mdb)_.*")
+        .size_t_is_usize(true)
         .ctypes_prefix("::libc")
         .blacklist_item("mode_t")
         .blacklist_item("filehandle_t")

lmdb-sys/src/bindings.rs
@@ -53,7 +53,7 @@ pub const MDB_BAD_VALSIZE: ::libc::c_int = -30781;
 pub const MDB_BAD_DBI: ::libc::c_int = -30780;
 pub const MDB_LAST_ERRCODE: ::libc::c_int = -30780;
 pub type mdb_mode_t = mode_t;
-pub type mdb_filehandle_t = filehandle_t;
+pub type mdb_filehandle_t = ::libc::c_int;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct MDB_env {
@@ -82,6 +82,7 @@ pub struct MDB_cursor {
 #[doc = " The same applies to data sizes in databases with the #MDB_DUPSORT flag."]
 #[doc = " Other data items can in theory be from 0 to 0xffffffff bytes long."]
 #[repr(C)]
+#[derive(Debug, Copy, Clone)]
 pub struct MDB_val {
     #[doc = "< size of the data item"]
     pub mv_size: usize,
@@ -167,6 +168,7 @@ pub const MDB_PREV_MULTIPLE: MDB_cursor_op = 18;
 pub type MDB_cursor_op = u32;
 #[doc = " @brief Statistics for a database in the environment"]
 #[repr(C)]
+#[derive(Debug, Copy, Clone)]
 pub struct MDB_stat {
     #[doc = "< Size of a database page."]
     #[doc = "This is currently the same for all databases."]
@@ -184,6 +186,7 @@ pub struct MDB_stat {
 }
 #[doc = " @brief Information about the environment"]
 #[repr(C)]
+#[derive(Debug, Copy, Clone)]
 pub struct MDB_envinfo {
     #[doc = "< Address of map, if fixed"]
     pub me_mapaddr: *mut ::libc::c_void,

lmdb-sys/Cargo.toml
@@ -28,7 +28,7 @@ libc = "0.2"
 [build-dependencies]
 pkg-config = "0.3"
 cc = "1.0"
-bindgen = { version = "0.51.1-oldsyn", default-features = false, optional = true }
+bindgen = { version = "0.53.2", default-features = false, optional = true }
 
 [features]
 default = []

mozilla/lmdb-rs

commit 3f1067ab6cabb705709af4df9a0ddd272e010d92

Fix bindings.
PR #74 introduced a manually-generated change to the bindings that I
accidentally broke in #76.

lmdb-sys/bindgen.rs
@@ -53,7 +53,7 @@ pub fn generate() {
         .size_t_is_usize(true)
         .ctypes_prefix("::libc")
         .blacklist_item("mode_t")
-        .blacklist_item("filehandle_t")
+        .blacklist_item("mdb_filehandle_t")
         .blacklist_item("^__.*")
         .parse_callbacks(Box::new(Callbacks {}))
         .layout_tests(false)

lmdb-sys/src/bindings.rs
@@ -53,7 +53,6 @@ pub const MDB_BAD_VALSIZE: ::libc::c_int = -30781;
 pub const MDB_BAD_DBI: ::libc::c_int = -30780;
 pub const MDB_LAST_ERRCODE: ::libc::c_int = -30780;
 pub type mdb_mode_t = mode_t;
-pub type mdb_filehandle_t = ::libc::c_int;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct MDB_env {

lmdb-sys/src/lib.rs
@@ -14,9 +14,9 @@ pub type mode_t = ::libc::c_int;
 
 #[cfg(unix)]
 #[allow(non_camel_case_types)]
-pub type filehandle_t = ::libc::c_int;
+pub type mdb_filehandle_t = ::libc::c_int;
 #[cfg(windows)]
 #[allow(non_camel_case_types)]
-pub type filehandle_t = *mut ::libc::c_void;
+pub type mdb_filehandle_t = *mut ::libc::c_void;
 
 include!("bindings.rs");