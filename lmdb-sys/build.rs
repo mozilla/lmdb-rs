5c845a5d43849e721db1d769e8817e5b33a17ae8
Paul H. Liu <paul@thev.net>
2020-12-08 22:56:08 -0800

    Use probe_library

--- lmdb-sys/build.rs
+++ lmdb-sys/build.rs
@@ -59,7 +59,7 @@ fn main() {
         warn!("Building with `-fsanitize=fuzzer`.");
     }
 
-    if !pkg_config::find_library("lmdb").is_ok() {
+    if !pkg_config::probe_library("lmdb").is_ok() {
         let mut builder = cc::Build::new();
 
         builder
